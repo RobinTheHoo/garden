f4bdae6fd7e98a1c20a90d155c5a32d5918bb2d8
RobinTheHoo <ved.dave7@gmail.com>
2024-09-05 10:18:37 +1000

    Aesthetic changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'http://localhost:5173/',
-    integrations: [sitemap()],
+  site: "http://localhost:5173/",
+  integrations: [sitemap()],
 });
